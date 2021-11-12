15a09c850ed08042f2ec40dfe13ff40ce2e608a0
Clémence Garnier <clemence.garnier@edu.esiee.fr>
2021-11-12 10:43:53 +0100

    maj page accueil (bouton membre fonctionnel)

--- app/src/main/java/com/example/appkenai/Accueil.java
+++ app/src/main/java/com/example/appkenai/Accueil.java
@@ -2,13 +2,27 @@ package com.example.appkenai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Accueil extends AppCompatActivity {
+
+    ImageView membres;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+
+        membres= findViewById(R.id.bouton_membres);
+        membres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Accueil.this, MembresActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/appkenai/MembresActivity.java
@@ -0,0 +1,14 @@
+package com.example.appkenai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MembresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_membres);
+    }
+}
