860957a8db057a6f0556fe173bc26249d9f4165d
colletjea <jean-paul.collet@edu.esiee.fr>
2021-12-09 13:19:57 +0100

    essai boutton les membres en haut à gauche

--- app/src/main/java/com/example/appkenai/Accueil.java
+++ app/src/main/java/com/example/appkenai/Accueil.java
@@ -2,27 +2,35 @@ package com.example.appkenai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class Accueil extends AppCompatActivity {
 
     ImageView membres;
+    private Button buttonAllerSurMembres;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
 
-        findViewById(R.id.boutonMembres).setOnClickListener(new View.OnClickListener() {
+        buttonAllerSurMembres = findViewById(R.id.boutonMembres);
+
+        buttonAllerSurMembres.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Accueil.this, MembresActivity.class));
+                //Intent intent = new Intent(MainActivity.this, MembresActivity.class);
+                //startActivity(intent);
+                setContentView(R.layout.activity_main);
+
             }
         });
-
 
 
 
--- app/src/main/java/com/example/appkenai/MainActivity.java
+++ app/src/main/java/com/example/appkenai/MainActivity.java
@@ -2,20 +2,29 @@ package com.example.appkenai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
+    //@Override
+    //public boolean onCreateOptionsMenu(Menu menu) {
+    //    getMenuInflater().inflate(R.menu.main_menu, menu);
+    //    return true;
+    //}
 }//test2
