a39ed4c6bcf767e0312c4c7ffa4988f7e71d7d1c
Clémence Garnier <clemence.garnier@edu.esiee.fr>
2021-12-16 11:15:16 +0100

    ajout page Membre X (pas reliée par bouton) + logo de l'appli

---
+++ app/src/main/java/com/example/appkenai/PresentationMembresX.java
@@ -0,0 +1,14 @@
+package com.example.appkenai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PresentationMembresX extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presentation_membres_x);
+    }
+}
